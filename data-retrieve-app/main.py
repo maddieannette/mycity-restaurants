d746338ef4dcfe2e139048c5574081982733a2b8
maddieannette <64941576+maddieannette@users.noreply.github.com>
2021-03-22 20:50:49 -0500

    Parsing columns

--- data-retrieve-app/main.py
+++ data-retrieve-app/main.py
@@ -1,5 +1,6 @@
 import requests
 import os
+import json
 # Step One: Identify city to use 
 city = 'Corpus Christi, Texas'
 
@@ -14,8 +15,22 @@ print(contents)
 # Pull data for city - resturant from yelp
 
 headers = {"Authorization": "Bearer " + contents}
-query = {'term':'food', 'location':city}
+query = {'term':'food', 'location':city, 'limit': 50}
 response = requests.get("https://api.yelp.com/v3/businesses/search", params=query, headers=headers)
-print(response.json())
+# print(response.json())
+
+x = response.json()
+# y = json.loads(x)
+places = x['businesses']
+
+count = 0
+
+
+
+for place in places:
+  count += 1
+  print(place['name'] + ' Is Closed: ' +str(place['is_closed' ]) + ' Rating: ' + str(place['rating']) +': ' + str(count))
+
+
 
 # Store in database
