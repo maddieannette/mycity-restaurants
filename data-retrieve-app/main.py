6f31002818478f062f018c72f89416322c6cae4a
maddieannette <64941576+maddieannette@users.noreply.github.com>
2021-05-01 21:37:52 -0500

    cross platform attempt

--- data-retrieve-app/main.py
+++ data-retrieve-app/main.py
@@ -20,9 +20,12 @@ c = conn.cursor()
 # Step One: Identify city to use 
 city = 'Corpus Christi, Texas'
 #Pull Api Key from ApiKeyFile
-apiKeyPath = os.getcwd() + "/data-retrieve-app/apikey.txt"
+# apiKeyPath = os.getcwd() + "/data-retrieve-app/apikey.txt"
 
-myfile = open(apiKeyPath, "rt") # open lorem.txt for reading text
+api_folder = os.path.dirname(os.path.abspath(__file__))
+api_file = os.path.join(api_folder, 'apikey.txt')
+myfile = open(api_file, "rt") 
+# open lorem.txt for reading text
 contents = myfile.read()         # read the entire file to string
 myfile.close()                   # close the file
 print(contents)
