1beb52ae15eff07ef9bc1fde2c038433e911b219
maddieannette <64941576+maddieannette@users.noreply.github.com>
2021-05-02 19:21:35 -0500

    final push before timeline

--- sqltest.sql
+++ sqltest.sql
@@ -1,2 +1,16 @@
 
-select count(*) from restaurants
+select with_review.* 
+from restaurants as with_review
+join (
+    select name,
+    longitude,
+    latitude,
+    max(created_at) as last_poll
+    from restaurants
+    group by name, longitude, latitude
+) as grouped_restaurants
+    on with_review.name = grouped_restaurants.name
+    and with_review.longitude = grouped_restaurants.longitude
+    and with_review.latitude = grouped_restaurants.latitude
+    and with_review.created_at = grouped_restaurants.last_poll
+    where with_review.name = 'Marble Slab Creamery'
